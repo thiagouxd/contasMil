d56e173f240f43878a0d2803b9bb48475974badf
thiagouxd <thiago.uxbr@gmail.com>
2022-01-10 23:07:17 -0300

    Inicio Projeto

---
+++ src/pages/Home/Card.tsx
@@ -0,0 +1,51 @@
+import styled from "styled-components"
+import Tag from "../../components/Tag"
+
+export default (props: { title: String, stipulated: String, used: String, balance: String }) => {
+  return (
+    <Container>
+      <Header>
+        <Title>{props.title}</Title>
+        <Tag text="Cagou nas Zorba" />
+      </Header>
+      <Description>
+        <p>Valor estipulado: <strong>R${props.stipulated}</strong></p>
+        <p>Saldo utilizado: <strong>R${props.used}</strong></p>
+        <p>Saldo para o dia: <strong>R${props.balance}</strong></p>
+      </Description>
+    </Container>
+  )
+}
+
+const Title = styled.h2`
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 22px;
+  color: #FFFFFF;
+  margin: 0;
+`
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Container = styled.div`
+  background: #565656;
+  border-radius: 0 8px 8px 0px;
+  margin-right: 24px;
+  padding: 16px;
+  margin-bottom: 16px;
+`
+
+const Description = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  p { 
+    margin: 0;
+    margin-right: 16px;
+    color: white;
+    font-size: 14px;
+  }
+`
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import Card from "./Card"
+
+export default () => {
+  return (
+    <>
+      <h1>Gastos infinitos</h1>
+      <Card title="Gasto"
+        stipulated="42,90"
+        used="42,90"
+        balance="42,90" />
+    </>
+  )
+}
